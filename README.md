fde61c50aed9f8c91299d0b754f50d08f317549e
王博闻 <wangbowen@mochasoft.com.cn>
2024-03-07 20:41:58 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# react-native-keyevent
+
+Config plugin to auto configure react-native-keyevent on prebuild
+### Add the package to your npm dependencies
+
+```
+yarn add react-native-keyevent
+yarn add --dev react-native-keyevent-expo-config-plugin
+```
+
+In your `app.json` or equivalent, add the following;
+```js
+{
+    expo: {
+      /* ... */
+      plugins: ['react-native-keyevent-expo-config-plugin'],
+    },
+  }
+```
+
+# Contributing
+
+Contributions are very welcome! Please refer to guidelines described in the [contributing guide]( https://github.com/expo/expo#contributing).
---
+++ src/withReactNativeKeyevent.ts
@@ -0,0 +1,210 @@
+import {
+  ConfigPlugin,
+  withAppDelegate,
+  withMainActivity,
+} from "@expo/config-plugins";
+import { mergeContents } from "@expo/config-plugins/build/utils/generateCode";
+
+const withIosAppDelegateImport: ConfigPlugin = (config) => {
+  // @ts-ignore
+  const newConfig = withAppDelegate(config, (config) => {
+    const newSrc = ["#import <RNKeyEvent.h>"];
+    const newConfig = mergeContents({
+      tag: "react-native-keyevent-import",
+      src: config.modResults.contents,
+      newSrc: newSrc.join("\n"),
+      anchor: `#import "AppDelegate.h"`,
+      offset: 1,
+      comment: "//",
+    });
+    return {
+      ...config,
+      modResults: newConfig,
+    };
+  });
+  return newConfig;
+};
+const withIosAppDelegateBody: ConfigPlugin = (config) => {
+  // @ts-ignore
+  const newConfig = withAppDelegate(config, (config) => {
+    const newSrc = [
+      "RNKeyEvent *keyEvent = nil;",
+      " ",
+      "- (NSMutableArray<UIKeyCommand *> *)keyCommands {",
+      "  NSMutableArray *keys = [NSMutableArray new];",
+      "   ",
+      "  if (keyEvent == nil) {",
+      "    keyEvent = [[RNKeyEvent alloc] init];",
+      "  }",
+      "   ",
+      "  if ([keyEvent isListening]) {",
+      '    NSArray *namesArray = [[keyEvent getKeys] componentsSeparatedByString:@","];',
+      "     ",
+      '    NSCharacterSet *validChars = [NSCharacterSet characterSetWithCharactersInString:@"ABCDEFGHIJKLMNOPQRSTUVWXYZ"];',
+      "     ",
+      "    for (NSString* names in namesArray) {",
+      "      NSRange  range = [names rangeOfCharacterFromSet:validChars];",
+      "       ",
+      "      if (NSNotFound != range.location) {",
+      "        [keys addObject: [UIKeyCommand keyCommandWithInput:names modifierFlags:UIKeyModifierShift action:@selector(keyInput:)]];",
+      "      } else {",
+      "        [keys addObject: [UIKeyCommand keyCommandWithInput:names modifierFlags:0 action:@selector(keyInput:)]];",
+      "      }",
+      "    }",
+      "  }",
+      "   ",
+      "  return keys;",
+      "}",
+      "",
+      "- (void)keyInput:(UIKeyCommand *)sender {",
+      "  NSString *selected = sender.input;",
+      "  [keyEvent sendKeyEvent:selected];",
+      "}",
+    ];
+    const newConfig = mergeContents({
+      tag: "react-native-keyevent-body",
+      src: config.modResults.contents,
+      newSrc: newSrc.join("\n"),
+      anchor: `@implementation AppDelegate`, // /#import "AppDelegate\.h"/g,
+      offset: 1,
+      comment: "//",
+    });
+    return {
+      ...config,
+      modResults: newConfig,
+    };
+  });
+  return newConfig;
+};
+
+const withAndroidMainActivityImport: ConfigPlugin = (config) => {
+  // @ts-ignore
+  const newConfig = withMainActivity(config, (config) => {
+    const newSrc = [
+      "import android.view.KeyEvent;",
+      "import com.github.kevinejohn.keyevent.KeyEventModule;",
+    ];
+    const newConfig = mergeContents({
+      tag: "react-native-keyevent-import",
+      src: config.modResults.contents,
+      newSrc: newSrc.join("\n"),
+      anchor: `;`,
+      offset: 1,
+      comment: "//",
+    });
+    return {
+      ...config,
+      modResults: newConfig,
+    };
+  });
+  return newConfig;
+};
+const withAndroidMainActivityBody: ConfigPlugin = (config) => {
+  // @ts-ignore
+  const newConfig = withMainActivity(config, (config) => {
+    const newSrc = [
+      "private boolean mCaps;",
+      "private StringBuilder sb = new StringBuilder();",
+      "",
+      "",
+      "public boolean isInputFromScanner(Context context, KeyEvent event) {",
+      "  if (event.getDevice() == null) {",
+      "    return false;",
+      "  }",
+      "  if (event.getKeyCode() == KeyEvent.KEYCODE_BACK || event.getKeyCode() == KeyEvent.KEYCODE_VOLUME_DOWN || event.getKeyCode() == KeyEvent.KEYCODE_VOLUME_UP) {",
+      "    return false;",
+      "  }",
+      "  if (event.getDevice().getSources() == (InputDevice.SOURCE_KEYBOARD | InputDevice.SOURCE_DPAD | InputDevice.SOURCE_CLASS_BUTTON)) {",
+      "    return false;",
+      "  }",
+      "  Configuration cfg = context.getResources().getConfiguration();",
+      "  return cfg.keyboard != Configuration.KEYBOARD_UNDEFINED;",
+      "}",
+      "",
+      "private void checkLetterStatus(KeyEvent event) {",
+      "  int keyCode = event.getKeyCode();",
+      "  if (keyCode == KeyEvent.KEYCODE_SHIFT_RIGHT || keyCode == KeyEvent.KEYCODE_SHIFT_LEFT) {",
+      "      if (event.getAction() == KeyEvent.ACTION_DOWN) {",
+      "          mCaps = true;",
+      "      } else {",
+      "          mCaps = false;",
+      "      }",
+      "  }",
+      "}",
+      "",
+      "",
+      "@Override",
+      "public boolean dispatchKeyEvent(KeyEvent event) {",
+      "  if (!isInputFromScanner(this, event)) {",
+      "    return super.dispatchKeyEvent(event);",
+      "  }",
+      "  int action = event.getAction();",
+      "  checkLetterStatus(event);",
+      "",
+      "  switch (action) {",
+      "    case KeyEvent.ACTION_DOWN:",
+      "        if (event.getKeyCode() == KeyEvent.KEYCODE_SHIFT_RIGHT || event.getKeyCode() == KeyEvent.KEYCODE_SHIFT_LEFT) {",
+      "        } else {",
+      "            if (event.getKeyCode() == KeyEvent.KEYCODE_ENTER) {",
+      "            } else {",
+      "                char x = getInputCode(event);",
+      "                sb.append(x);",
+      "            }",
+      "        }",
+      "        if (event.getKeyCode() == KeyEvent.KEYCODE_VOLUME_UP) {",
+      "            return super.dispatchKeyEvent(event);",
+      "        }",
+      "        if (event.getKeyCode() == KeyEvent.KEYCODE_VOLUME_DOWN) {",
+      "            return super.dispatchKeyEvent(event);",
+      "        }",
+      "        if (event.getKeyCode() == KeyEvent.KEYCODE_BACK) {",
+      "            return super.dispatchKeyEvent(event);",
+      "        }",
+      "        if (event.getKeyCode() == KeyEvent.KEYCODE_MENU) {",
+      "            return super.dispatchKeyEvent(event);",
+      "        }",
+      "        if (event.getKeyCode() == KeyEvent.KEYCODE_HOME) {",
+      "            return super.dispatchKeyEvent(event);",
+      "        }",
+      "        if (event.getKeyCode() == KeyEvent.KEYCODE_POWER) {",
+      "            return super.dispatchKeyEvent(event);",
+      "        }",
+      "        final int len = sb.length();",
+      "        if (DisplayUtil.isConfirmKey(event.getKeyCode())) {",
+      "          KeyEventModule.getInstance().onKeyDownEvent(sb.toString(), event);",
+      "          if (sb.length() > 0) {",
+      "              sb.setLength(0);",
+      "          }",
+      "        }",
+      "        return true;",
+      "    default:",
+      "      break;",
+      "  }",
+      "  return super.dispatchKeyEvent(event);",
+      "}",
+    ];
+    const newConfig = mergeContents({
+      tag: "react-native-keyevent-body",
+      src: config.modResults.contents,
+      newSrc: newSrc.join("\n"),
+      anchor: `public class MainActivity extends ReactActivity {`,
+      offset: 1,
+      comment: "//",
+    });
+    return {
+      ...config,
+      modResults: newConfig,
+    };
+  });
+  return newConfig;
+};
+
+const initPlugin: ConfigPlugin = (config) => {
+  config = withIosAppDelegateImport(config);
+  config = withIosAppDelegateBody(config);
+  config = withAndroidMainActivityImport(config);
+  config = withAndroidMainActivityBody(config);
+  return config;
+};
+
+export default initPlugin;
